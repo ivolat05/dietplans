d9ab5f83006f047eaa1929017d7f3129258b44ae
Andrei Labadaev <andrei-labadaev@mail.ru>
2024-11-17 22:14:48 +0300

    uppdate

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,2 +1,8 @@
+--include('vendor/gsap.min.js')
+--include('vendor/ScrollTrigger.min.js')
 --include('vendor/swiper-bundle.min.js')
+--include('vendor/fslightbox.js')
 --include('components/modal.js')
+--include('components/animation.js')
+ 
+
---
+++ src/assets/js/components/animation.js
@@ -0,0 +1,50 @@
+const paralaxAnimBloclk = document.querySelectorAll(".anim-block-1");
+const paralaxAnimBloclkTwo = document.querySelectorAll(".anim-block-2");
+
+paralaxAnimation(paralaxAnimBloclk, 0.01, 1, -1);
+paralaxAnimation(paralaxAnimBloclkTwo, 0.01, -1, -1);
+
+/**
+ * Анимация параллакса для элементов на странице с использованием GSAP.
+ *
+ * @param {HTMLElement[]} animationElementAll - Массив элементов, которые нужно анимировать.
+ * @param {number} [amplitude=0.01] - Амплитуда параллакса, определяет степень смещения элементов. Чем больше значение, тем сильнее эффект.
+ * @param {number} [orientirX=1] - Направление смещения по оси X относительно курсора. Может быть `1` (стандартное направление) или `-1` (инверсия).
+ * @param {number} [orientirY=1] - Направление смещения по оси Y относительно курсора. Может быть `1` (стандартное направление) или `-1` (инверсия).
+ */
+
+function paralaxAnimation(
+	animationElementAll,
+	amplitude = 0.01,
+	orientirX = 1,
+	orientirY = 1
+) {
+	if (animationElementAll && animationElementAll.length > 0) {
+		let requestId = null;
+
+		document.addEventListener("mousemove", (e) => {
+			const mouseX = e.clientX;
+			const mouseY = e.clientY;
+
+			if (!requestId) {
+				requestId = requestAnimationFrame(() => {
+					animationElementAll.forEach((animationElement) => {
+						gsap.to(animationElement, {
+							x:
+								orientirX *
+								(mouseX - window.innerWidth / 2) *
+								amplitude,
+							y:
+								orientirY *
+								(mouseY - window.innerHeight / 2) *
+								amplitude,
+							ease: "power2.out",
+							duration: 0.3,
+						});
+					});
+					requestId = null;
+				});
+			}
+		});
+	}
+}
